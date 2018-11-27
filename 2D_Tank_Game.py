188107bd012e560471e69b3bebc5805744eaa82a
Alex Bautista <arb15s@my.fsu.edu>
2018-11-27 11:24:15 -0500

    Added OOP functionality for TANK. Also added player one and player two (Red_Tank_Turret.png)

--- 2D_Tank_Game.py
+++ 2D_Tank_Game.py
@@ -121,17 +121,29 @@ def game_intro():
         pygame.display.update()
         fpsClock.tick(FPS)
 
+class tank():
+    def __init__(self, image, _x, _y):
+        self.char = pygame.image.load(image)
+        self.x = _x
+        self.y = _y
+        self.prev_char_pos = 1
+        self.cur_char_pos = 1
+        self.vel = 10
+
 
 # Main Loop
 def main_loop():
     # character traits
     # needs to be OOP-ed
-    char = pygame.image.load('Tank_Turret.png')
-    x = screenWidth/2
-    y = screenHeight/2
-    prev_char_pos = 1
-    cur_char_pos = 1
-    vel = 10
+    #char = pygame.image.load('Tank_Turret.png')
+    #x = screenWidth/2
+    #y = screenHeight/2
+    #prev_char_pos = 1
+    #cur_char_pos = 1
+    #vel = 10
+
+    f_player = tank('Tank_Turret.png', screenWidth/2, screenHeight/2)
+    s_player = tank('Red_Tank_Turret.png', 50, 50)
 
     running = True
     while running:
@@ -143,70 +155,138 @@ def main_loop():
 
         keys = pygame.key.get_pressed()
 
-        # may have to write our own rotate functions. transform.rotate doesn't
-        # work well with anything small than 90 degrees.
-        if keys[pygame.K_a] and x > 0:
-            char = pygame.transform.rotate(char, 90)
-            if prev_char_pos == 1:
-                cur_char_pos = 4
-            if prev_char_pos == 2:
-                cur_char_pos = 1
-            if prev_char_pos == 3:
-                cur_char_pos = 2
-            if prev_char_pos == 4:
-                cur_char_pos = 3
-            prev_char_pos = cur_char_pos
+        # First Player Programming
+        if keys[pygame.K_a] and f_player.x > 0:
+            f_player.char = pygame.transform.rotate(f_player.char, 90)
+            if f_player.prev_char_pos == 1:
+                f_player.cur_char_pos = 4
+            if f_player.prev_char_pos == 2:
+                f_player.cur_char_pos = 1
+            if f_player.prev_char_pos == 3:
+                f_player.cur_char_pos = 2
+            if f_player.prev_char_pos == 4:
+                f_player.cur_char_pos = 3
+            f_player.prev_char_pos = f_player.cur_char_pos
             pygame.time.delay(150)
-        if keys[pygame.K_d] and x < screenWidth - width:
-            char = pygame.transform.rotate(char, 270)
-            if prev_char_pos == 1:
-                cur_char_pos = 2
-            if prev_char_pos == 2:
-                cur_char_pos = 3
-            if prev_char_pos == 3:
-                cur_char_pos = 4
-            if prev_char_pos == 4:
-                cur_char_pos = 1
+        if keys[pygame.K_d] and f_player.x < screenWidth - width:
+            f_player.char = pygame.transform.rotate(f_player.char, 270)
+            if f_player.prev_char_pos == 1:
+                f_player.cur_char_pos = 2
+            if f_player.prev_char_pos == 2:
+                f_player.cur_char_pos = 3
+            if f_player.prev_char_pos == 3:
+                f_player.cur_char_pos = 4
+            if f_player.prev_char_pos == 4:
+                f_player.cur_char_pos = 1
 
-            prev_char_pos = cur_char_pos
+            f_player.prev_char_pos = f_player.cur_char_pos
             pygame.time.delay(150)
         # also need to change what forward and backwards modify when rotated
         if keys[pygame.K_w]:
-            if cur_char_pos == 1:
-                y -= vel
-                player.move(0, vel * -1)
-            if cur_char_pos == 2:
-                x += vel
-                player.move(vel, 0)
-            if cur_char_pos == 3:
-                y += vel
-                player.move(0, vel)
-            if cur_char_pos == 4:
-                x -= vel
-                player.move(vel * -1, 0)
+            if f_player.cur_char_pos == 1:
+                f_player.y -= f_player.vel
+                player.move(0, f_player.vel * -1)
+            if f_player.cur_char_pos == 2:
+                f_player.x += f_player.vel
+                player.move(f_player.vel, 0)
+            if f_player.cur_char_pos == 3:
+                f_player.y += f_player.vel
+                player.move(0, f_player.vel)
+            if f_player.cur_char_pos == 4:
+                f_player.x -= f_player.vel
+                player.move(f_player.vel * -1, 0)
         if keys[pygame.K_s]:
-            if cur_char_pos == 1:
-                y += vel
-                player.move(0, vel)
-            if cur_char_pos == 2:
-                x -= vel
-                player.move(vel * -1, 0)
-            if cur_char_pos == 3:
-                y -= vel
-                player.move(0, vel * -1)
-            if cur_char_pos == 4:
-                x += vel
-                player.move(vel, 0)
+            if f_player.cur_char_pos == 1:
+                f_player.y += f_player.vel
+                player.move(0, f_player.vel)
+            if f_player.cur_char_pos == 2:
+                f_player.x -= f_player.vel
+                player.move(f_player.vel * -1, 0)
+            if f_player.cur_char_pos == 3:
+                f_player.y -= f_player.vel
+                player.move(0, f_player.vel * -1)
+            if f_player.cur_char_pos == 4:
+                f_player.x += f_player.vel
+                player.move(f_player.vel, 0)
 
         # check for screen bound in case wall collision doesn't work
-        if x <= -10:
-            x = 0
-        if y <= -10:
-            y = 0
-        if x >= screenWidth - width:
-            x = screenWidth - width
-        if y >= screenHeight - height:
-            y = screenHeight - height
+        if f_player.x <= -10:
+            f_player.x = 0
+        if f_player.y <= -10:
+            f_player.y = 0
+        if f_player.x >= screenWidth - width:
+            f_player.x = screenWidth - width
+        if f_player.y >= screenHeight - height:
+           f_player.y = screenHeight - height
+
+        # end first player programming
+
+        # second player programming
+
+        if keys[pygame.K_LEFT] and s_player.x > 0:
+            s_player.char = pygame.transform.rotate(s_player.char, 90)
+            if s_player.prev_char_pos == 1:
+                s_player.cur_char_pos = 4
+            if s_player.prev_char_pos == 2:
+                s_player.cur_char_pos = 1
+            if s_player.prev_char_pos == 3:
+                s_player.cur_char_pos = 2
+            if s_player.prev_char_pos == 4:
+                s_player.cur_char_pos = 3
+            s_player.prev_char_pos = s_player.cur_char_pos
+            pygame.time.delay(150)
+        if keys[pygame.K_RIGHT] and s_player.x < screenWidth - width:
+            s_player.char = pygame.transform.rotate(s_player.char, 270)
+            if s_player.prev_char_pos == 1:
+                s_player.cur_char_pos = 2
+            if s_player.prev_char_pos == 2:
+                s_player.cur_char_pos = 3
+            if s_player.prev_char_pos == 3:
+                s_player.cur_char_pos = 4
+            if s_player.prev_char_pos == 4:
+                s_player.cur_char_pos = 1
+
+            s_player.prev_char_pos = s_player.cur_char_pos
+            pygame.time.delay(150)
+        # also need to change what forward and backwards modify when rotated
+        if keys[pygame.K_UP]:
+            if s_player.cur_char_pos == 1:
+                s_player.y -= s_player.vel
+                #player.move(0, s_player.vel * -1)
+            if s_player.cur_char_pos == 2:
+                s_player.x += s_player.vel
+                #player.move(s_player.vel, 0)
+            if s_player.cur_char_pos == 3:
+                s_player.y += s_player.vel
+                #player.move(0, s_player.vel)
+            if s_player.cur_char_pos == 4:
+                s_player.x -= s_player.vel
+                #player.move(s_player.vel * -1, 0)
+        if keys[pygame.K_DOWN]:
+            if s_player.cur_char_pos == 1:
+                s_player.y += s_player.vel
+                #player.move(0, s_player.vel)
+            if s_player.cur_char_pos == 2:
+                s_player.x -= s_player.vel
+                #player.move(s_player.vel * -1, 0)
+            if s_player.cur_char_pos == 3:
+                s_player.y -= s_player.vel
+                #player.move(0, s_player.vel * -1)
+            if s_player.cur_char_pos == 4:
+                s_player.x += s_player.vel
+                #player.move(s_player.vel, 0)
+
+        # check for screen bound in case wall collision doesn't work
+        if s_player.x <= -10:
+            s_player.x = 0
+        if s_player.y <= -10:
+            s_player.y = 0
+        if s_player.x >= screenWidth - width:
+            s_player.x = screenWidth - width
+        if s_player.y >= screenHeight - height:
+           s_player.y = screenHeight - height
+
+        #end second player programming
 
         # get rid of win.fill when background line is added
         win.fill(WHITE)
@@ -220,7 +300,8 @@ def main_loop():
         # draw hit box
         pygame.draw.rect(win, WHITE, player.rect)
         # draw character sprite
-        win.blit(char, (player.rect.centerx - 25, player.rect.centery - 25))
+        win.blit(f_player.char, (player.rect.centerx - 25, player.rect.centery - 25))
+        win.blit(s_player.char, (s_player.x, s_player.y))
         pygame.display.update()
         fpsClock.tick(FPS)
 
