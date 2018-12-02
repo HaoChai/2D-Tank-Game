4615c025d5a2a176a1d23943dcf7dd1a839cc57d
HaoChai <33661993+haochai@users.noreply.github.com>
2018-12-02 14:47:11 -0500

    add a speedup power-up, add the base for other

--- 2D_Tank_Game.py
+++ 2D_Tank_Game.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import pygame
 
@@ -18,6 +19,8 @@ width = 51
 height = 53
 player1_sprite = 'Tank_Turret.png'
 player2_sprite = 'Red_Tank_Turret.png'
+item_sprites = [pygame.image.load("item1.png"), pygame.image.load("item2.png"), pygame.image.load("item3.png"),
+                pygame.image.load("item4.png"), pygame.image.load("item5.png")]
 
 # colors
 BLACK = (0, 0, 0)
@@ -33,14 +36,30 @@ DOWN = 3
 LEFT = 4
 
 
+class Timer:
+    def __init__(self):
+        self.start_time = pygame.time.get_ticks()
+
+    def time_diff(self):
+        seconds = (pygame.time.get_ticks() - self.start_time) / 1000
+        seconds = round(seconds)
+        return seconds
+
+    def reset_timer(self):
+        self.start_time = pygame.time.get_ticks()
+
+
 # reference: https://www.pygame.org/project-Rect+Collision+Response-1061-.html
 class Tank:
-    def __init__(self, image):
+    def __init__(self, image, num):
+        self.player_number = num
         self.rect = pygame.Rect(width, width, height, height)
         self.char = pygame.image.load(image)
         self.vel = 10
         self.prev_char_pos = UP
         self.cur_char_pos = UP
+        self.timers = [Timer(), Timer(), Timer(), Timer(), Timer()]
+        self.speed_up = False
 
     def move(self, x, y):
         if x != 0:
@@ -62,6 +81,37 @@ class Tank:
                     self.rect.bottom = wall.rect.top
                 if y < 0:
                     self.rect.top = wall.rect.bottom
+
+        # check player collision
+        check_player = player2
+        if self.player_number == 2:
+            check_player = player1
+        if self.rect.colliderect(check_player.rect):
+            if x > 0:
+                self.rect.right = check_player.rect.left
+            if x < 0:
+                self.rect.left = check_player.rect.right
+            if y > 0:
+                self.rect.bottom = check_player.rect.top
+            if y < 0:
+                self.rect.top = check_player.rect.bottom
+
+        # check item collision
+        for item in items:
+            if self.rect.colliderect(item.rect):
+                if item.item_type == 0 and item.spawned is True:
+                    self.speed_up = True
+                    self.timers[0].reset_timer()
+                    self.vel = 16
+                item.reset_item()
+
+        self.power_up_handler()
+
+    def power_up_handler(self):
+        if self.speed_up is True:
+            if self.timers[0].time_diff() == 5:
+                self.vel = 10
+                self.speed_up = False
 
     def rotate_left(self):
         self.char = pygame.transform.rotate(self.char, 90)
@@ -114,6 +164,31 @@ class Tank:
         win.blit(self.char, (self.rect.centerx - 25, self.rect.centery - 25))
 
 
+class Item:
+    def __init__(self, pos):
+        items.append(self)
+        self.item_type = random.randint(0, 4)
+        self.sprite = item_sprites[self.item_type]
+        self.rect = pygame.Rect(boxSize, boxSize, boxSize, boxSize)
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
+        self.timer = Timer()
+        self.spawned = False
+        self.cool_down = 5
+
+    def draw_item(self):
+        if self.timer.time_diff() == self.cool_down or self.spawned is True:
+            pygame.draw.rect(win, WHITE, self.rect)
+            win.blit(self.sprite, (self.rect.x + 4, self.rect.y + 1))
+            self.spawned = True
+
+    def reset_item(self):
+        self.item_type = random.randint(0, 4)
+        self.sprite = item_sprites[self.item_type]
+        self.timer.reset_timer()
+        self.spawned = False
+        self.cool_down = random.randint(5, 15)
+
 class Wall:
     def __init__(self, pos):
         walls.append(self)
@@ -124,8 +199,9 @@ class Wall:
 bg = pygame.image.load("background.jpg")
 
 walls = []
-player1 = Tank(player1_sprite)
-player2 = Tank(player2_sprite)
+items = []
+player1 = Tank(player1_sprite, 1)
+player2 = Tank(player2_sprite, 2)
 fpsClock = pygame.time.Clock()
 
 level = [
@@ -133,7 +209,7 @@ level = [
     "W             W",
     "W             W",
     "W             W",
-    "W1           2W",
+    "W1     I     2W",
     "W             W",
     "W             W",
     "W             W",
@@ -161,6 +237,8 @@ def setup_level():
             if col == "2":
                 player2.rect.x = x
                 player2.rect.y = y
+            if col == "I":
+                Item((x, y))
             x += boxSize
         y += boxSize
         x = 0
@@ -229,6 +307,7 @@ def main_loop():
         # background needs to be updated here, after its set at the top
         win.blit(bg, (0, 0))
 
+        # draw grid
         draw_grid()
         # draw walls
         for wall in walls:
@@ -236,6 +315,11 @@ def main_loop():
         # draw player
         player1.draw_tank()
         player2.draw_tank()
+
+        # draw item
+        for item in items:
+            item.draw_item()
+
         pygame.display.update()
         fpsClock.tick(FPS)
 
